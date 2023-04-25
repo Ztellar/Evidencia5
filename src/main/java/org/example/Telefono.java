46a08eff49583e6b2d02c0242a5a136aa08b5b11
Ztellar <b.salamanca01@ufromail.cl>
2023-04-24 22:24:20 -0400

    Clase Teléfono

---
+++ src/main/java/org/example/Telefono.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Telefono {
+    private int numero;
+    private String modelo;
+    private String marca;
+    private String color;
+    private int añoLanzamiento;
+    private String cantidadCamaras;
+    private String tamañoPantalla;
+    private String tipoTelefono;
+    public void llamar(){
+        // Metodo para llamar
+    }
+    public void cargarTelefono(){
+        // Metodo para cargar telefono
+    }
+    public void mostrarNumero(){
+        // Metodo para mostrar numero
+    }
+    public void jugar(){
+        // Metodo para jugar
+    }
+    public void verVideos(){
+        // Metodo para ver videos
+    }
+    public void mostrarBateria(){
+        // Metodo para mostrar bateria
+    }
+    public void chatear(){
+        // Metodo para chatear
+    }
+}
